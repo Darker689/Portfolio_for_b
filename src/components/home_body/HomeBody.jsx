168095bb4aceecbcae61e30de307b98821fa82ea
Darker689 <muhammadjon13689@gmail.com>
2023-03-16 07:05:54 +0500

    Success 3

--- src/components/home_body/HomeBody.jsx
+++ src/components/home_body/HomeBody.jsx
@@ -1,7 +1,8 @@
 // router
 import { Link } from 'react-router-dom';
 // icons
-import {AiFillHeart} from 'react-icons/ai'
+import { AiFillHeart, AiFillEye } from 'react-icons/ai'
+import { HiShare } from 'react-icons/hi'
 import { FaFacebookMessenger } from 'react-icons/fa'
 // img
 import img from '../../assets/images/nav_img/nav_user.png'
@@ -18,91 +19,232 @@ const Home_body = () => {
           <Link to='/'>All</Link>
           <Link to='/'>Following</Link>
           <Link to='/'>Newest</Link>
-          <Link to='/'>Popular</Link> 
+          <Link to='/'>Popular</Link>
         </div>
       </div>
       <div className="home_body_posts">
-        <Link to='/product' className="home_body_post">
-          <div className="home_body_post_img">
-            <img src={img1} alt="" />
-          </div>
-          <div className="home_body_post_bottom">
-            <div className="home_body_post_bottom_user">
-              <div className="home_body_post_bottom_user_img">
-                <img src={img} alt="" />
-              </div>
-              <p>John Doe</p>
-            </div>
-            <div className="home_body_post_bottom_heart">
-              <i><AiFillHeart/></i>
-              <p>100</p>
-            </div>
-            <div className="home_body_post_bottom_message">
-              <i><FaFacebookMessenger/></i>
-              <p>100</p>
-            </div>
-          </div>
-        </Link>
         <div className="home_body_post">
-          <div className="home_body_post_img">
-            <img src={img1} alt="" />
-          </div>
-          <div className="home_body_post_bottom">
-            <div className="home_body_post_bottom_user">
-              <div className="home_body_post_bottom_user_img">
-                <img src={img} alt="" />
-              </div>
-              <p>John Doe</p>
+          <div className="home_body_post_bottom_user">
+            <div className="home_body_post_bottom_user_img">
+              <img src={img} alt="" />
             </div>
+            <div>
+              <p>John Doe</p>
+              <small>Data</small>
+            </div>
+          </div>
+          <div className="home_body_post_txt">
+            <p className="home_body_post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dignissimos!</p>
+            <p className="home_body_post_tags">
+              <span>
+                <i>#</i> 
+                <p>dragon</p>
+              </span>
+              <span>
+                <i>#</i> 
+                <p>dragons</p>
+              </span>
+            </p>
+          </div>
+          <Link to='/' className="home_body_post_img">
+            <img src={img1} alt="" />
+          </Link>
+          <div className="home_body_post_bottom">
             <div className="home_body_post_bottom_heart">
-              <i><AiFillHeart/></i>
+              <i><AiFillHeart /></i>
               <p>100</p>
             </div>
             <div className="home_body_post_bottom_message">
-              <i><FaFacebookMessenger/></i>
+              <i><FaFacebookMessenger /></i>
               <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><AiFillEye /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><HiShare /></i>
+              <p>share</p>
             </div>
           </div>
         </div>
         <div className="home_body_post">
-          <div className="home_body_post_img">
-            <img src={img1} alt="" />
-          </div>
-          <div className="home_body_post_bottom">
-            <div className="home_body_post_bottom_user">
-              <div className="home_body_post_bottom_user_img">
-                <img src={img} alt="" />
-              </div>
-              <p>John Doe</p>
+          <div className="home_body_post_bottom_user">
+            <div className="home_body_post_bottom_user_img">
+              <img src={img} alt="" />
             </div>
+            <div>
+              <p>John Doe</p>
+              <small>Data</small>
+            </div>
+          </div>
+          <div className="home_body_post_txt">
+            <p className="home_body_post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dignissimos!</p>
+            <p className="home_body_post_tags">
+              <span>
+                <i>#</i> 
+                <p>dragon</p>
+              </span>
+              <span>
+                <i>#</i> 
+                <p>dragons</p>
+              </span>
+            </p>
+          </div>
+          <Link to='/' className="home_body_post_img">
+            <img src={img1} alt="" />
+          </Link>
+          <div className="home_body_post_bottom">
             <div className="home_body_post_bottom_heart">
-              <i><AiFillHeart/></i>
+              <i><AiFillHeart /></i>
               <p>100</p>
             </div>
             <div className="home_body_post_bottom_message">
-              <i><FaFacebookMessenger/></i>
+              <i><FaFacebookMessenger /></i>
               <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><AiFillEye /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><HiShare /></i>
+              <p>share</p>
             </div>
           </div>
         </div>
         <div className="home_body_post">
-          <div className="home_body_post_img">
-            <img src={img1} alt="" />
-          </div>
-          <div className="home_body_post_bottom">
-            <div className="home_body_post_bottom_user">
-              <div className="home_body_post_bottom_user_img">
-                <img src={img} alt="" />
-              </div>
-              <p>John Doe</p>
+          <div className="home_body_post_bottom_user">
+            <div className="home_body_post_bottom_user_img">
+              <img src={img} alt="" />
             </div>
+            <div>
+              <p>John Doe</p>
+              <small>Data</small>
+            </div>
+          </div>
+          <div className="home_body_post_txt">
+            <p className="home_body_post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dignissimos!</p>
+            <p className="home_body_post_tags">
+              <span>
+                <i>#</i> 
+                <p>dragon</p>
+              </span>
+              <span>
+                <i>#</i> 
+                <p>dragons</p>
+              </span>
+            </p>
+          </div>
+          <Link to='/' className="home_body_post_img">
+            <img src={img1} alt="" />
+          </Link>
+          <div className="home_body_post_bottom">
             <div className="home_body_post_bottom_heart">
-              <i><AiFillHeart/></i>
+              <i><AiFillHeart /></i>
               <p>100</p>
             </div>
             <div className="home_body_post_bottom_message">
-              <i><FaFacebookMessenger/></i>
+              <i><FaFacebookMessenger /></i>
               <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><AiFillEye /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><HiShare /></i>
+              <p>share</p>
+            </div>
+          </div>
+        </div>
+        <div className="home_body_post">
+          <div className="home_body_post_bottom_user">
+            <div className="home_body_post_bottom_user_img">
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <p>John Doe</p>
+              <small>Data</small>
+            </div>
+          </div>
+          <div className="home_body_post_txt">
+            <p className="home_body_post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dignissimos!</p>
+            <p className="home_body_post_tags">
+              <span>
+                <i>#</i> 
+                <p>dragon</p>
+              </span>
+              <span>
+                <i>#</i> 
+                <p>dragons</p>
+              </span>
+            </p>
+          </div>
+          <Link to='/' className="home_body_post_img">
+            <img src={img1} alt="" />
+          </Link>
+          <div className="home_body_post_bottom">
+            <div className="home_body_post_bottom_heart">
+              <i><AiFillHeart /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><FaFacebookMessenger /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><AiFillEye /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><HiShare /></i>
+              <p>share</p>
+            </div>
+          </div>
+        </div>
+        <div className="home_body_post">
+          <div className="home_body_post_bottom_user">
+            <div className="home_body_post_bottom_user_img">
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <p>John Doe</p>
+              <small>Data</small>
+            </div>
+          </div>
+          <div className="home_body_post_txt">
+            <p className="home_body_post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dignissimos!</p>
+            <p className="home_body_post_tags">
+              <span>
+                <i>#</i> 
+                <p>dragon</p>
+              </span>
+              <span>
+                <i>#</i> 
+                <p>dragons</p>
+              </span>
+            </p>
+          </div>
+          <Link to='/' className="home_body_post_img">
+            <img src={img1} alt="" />
+          </Link>
+          <div className="home_body_post_bottom">
+            <div className="home_body_post_bottom_heart">
+              <i><AiFillHeart /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><FaFacebookMessenger /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><AiFillEye /></i>
+              <p>100</p>
+            </div>
+            <div className="home_body_post_bottom_message">
+              <i><HiShare /></i>
+              <p>share</p>
             </div>
           </div>
         </div>
